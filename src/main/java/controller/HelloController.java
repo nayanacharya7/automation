081054126d6559ee7c46f8f896d833565a7cb36b
ADMIN <nayna7acharya@gmail.com>
2025-04-29 18:02:04 +0530

    docker

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping("/")
+  public String hello() {
+    return "Hello from Spring Boot with CI/CD! aa aaaaaa ddsa aaa entry hdfdsafdsafdfda dfsvfvdsfda ewfewfawweaewasa";
+  }
+}
