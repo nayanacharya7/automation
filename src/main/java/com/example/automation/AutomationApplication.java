2dd00dc93e0e1bd792c8ffa051cb50bbb58a89bc
ADMIN <nayna7acharya@gmail.com>
2025-05-06 18:22:22 +0530

    call

--- src/main/java/com/example/automation/AutomationApplication.java
+++ src/main/java/com/example/automation/AutomationApplication.java
@@ -9,7 +9,7 @@ public class AutomationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutomationApplication.class, args);
         System.out.println(" mmm AA DD QQ QQ");
-
+        System.out.println(" mmm AA DD QQ QQ");
 
     }
 
